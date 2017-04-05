aa077e1a580860015ca09fdb5d0804c6559a4ddb
Jérémy Van de Wyngaert <jeremyvdw@protonmail.com>
2017-04-05 14:46:30 +0200

    Bumped clojure, component and HikariCP versions

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/weavejester/duct-hikaricp-component"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.stuartsierra/component "0.2.2"]
-                 [com.zaxxer/HikariCP-java6 "2.2.5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [com.zaxxer/HikariCP "2.6.1"]
                  [org.slf4j/slf4j-nop "1.7.7"]]
   :profiles
   {:dev {:dependencies [[org.clojure/java.jdbc "0.3.6"]
